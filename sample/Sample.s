06aadfa58cd8014bd14ce38c62988e7a88b639fb
Dmytro <dm.vict.kr@gmail.com>
2020-12-22 10:09:15 +0200

    Improve sample `Sample.s`

--- sample/Sample.s
+++ sample/Sample.s
@@ -1,5 +1,9 @@
-let _ = require( '../' )
+if( typeof module !== 'undefined' )
+require( 'wmodulefortesting12' );
+
+let _ = _global_._test_;
 
 var result = _.divideMulOnSum( 4, 4 );
 
-console.log( result );
+console.log( `The result of dividing the multiplication of 4 and 4 on sum of 4 and 4 is : ${ result }` );
+/* log : The result of dividing the multiplication of 4 and 4 on sum of 4 and 4 is : 2 */
